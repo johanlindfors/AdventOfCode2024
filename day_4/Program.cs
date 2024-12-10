ebd03c1a1061d8b1d2e1300a63bc2db1eff519fe
Johan Lindfors <Johan.Lindfors@coderox.se>
2024-12-10 09:42:50 +0100

    Prepared for day 4

---
+++ day_4/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    struct Point
+    {
+        public int x;
+        public int y;
+    }
+
+    struct Path
+    {
+        List<Point> points;
+    }
+
+    private static void Main(string[] args)
+    {
+        var data = File.ReadAllLines("test-data1.txt");
+
+        searchpaths = new List<Path>() {
+            
+        }
+
+foreach (string line in data)
+        {
+
+        }
+    }
+}
